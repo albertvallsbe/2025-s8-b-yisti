9f5c4013cf7f08d8587001d643a3f69ade61387f
Albert <avalls89@gmail.com>
2025-09-07 01:04:37 +0200

    Update config

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,21 +12,22 @@ export const requireEnv = (envVarName: string, fallback?: string): string => {
 };
 
 const env = requireEnv("NODE_ENV", "development");
+const isProd = env === "production" ? true : false;
 
 export const config = {
 	env,
-	isProd: env === "production" ? true : false,
-	port: Number(requireEnv("PORT", "3100")),
-	dbUser: requireEnv("DB_USER"),
-	dbPassword: requireEnv("DB_PASSWORD"),
-	dbHost: requireEnv("DB_HOST"),
-	dbName: requireEnv("DB_NAME"),
-	dbPort: Number(requireEnv("DB_PORT", "5432")),
-	dbUrl: requireEnv("DATABASE_URL", ""),
+	isProd,
+	databaseUrl: requireEnv("DATABASE_URL", ""),
 	jwtSecret: requireEnv("JWT_SECRET"),
 	apiKey: requireEnv("API_KEY"),
 	googleClientId: requireEnv("GOOGLE_CLIENT_ID"),
 	googleClientSecret: requireEnv("GOOGLE_CLIENT_SECRET"),
 	googleRefreshToken: requireEnv("GOOGLE_REFRESH_TOKEN"),
 	googleUser: requireEnv("GOOGLE_USER"),
+	port: isProd ? undefined : Number(requireEnv("PORT", "3100")),
+	dbUser: isProd ? undefined : requireEnv("DB_USER"),
+	dbPassword: isProd ? undefined : requireEnv("DB_PASSWORD"),
+	dbHost: isProd ? undefined : requireEnv("DB_HOST"),
+	dbName: isProd ? undefined : requireEnv("DB_NAME"),
+	dbPort: isProd ? undefined : Number(requireEnv("DB_PORT", "5432")),
 };
